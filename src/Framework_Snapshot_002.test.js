9c5dae1b5be7b3ef808fcc7aaa81f4730154b437
sandeep-vedam <sandeepkumar.vedam@soctronics.com>
2019-11-20 19:00:55 +0530

    Added Framework and Webkit related test cases

---
+++ src/Framework_Snapshot_002.test.js
@@ -0,0 +1,43 @@
+import {
+  pluginActivate,
+  pluginDeactivate,
+  restartFramework,
+  screenshot,
+  webKitBrowserOps,
+} from './commonMethods/commonFunctions'
+import constants from './commonMethods/constants'
+
+export default {
+  title: 'Framework snapshot test with multiple start/stops',
+  description: 'Tests if the Framework snapshot module works',
+  repeat: 30,
+  setup: webKitBrowserOps,
+  teardown: restartFramework,
+  steps: [
+    {
+      description: 'Deactivating Snapshot Plugin',
+      test: pluginDeactivate,
+      params: constants.snapshotPlugin,
+      assert: 'deactivated',
+    },
+    {
+      description: 'Activating Snapshot Plugin',
+      test: pluginActivate,
+      params: constants.snapshotPlugin,
+      assert: 'activated',
+    },
+    {
+      description: 'Get a screenshot',
+      test: screenshot,
+    },
+  ],
+  validate() {
+    let resp = this.$data.read('screenshotResult')
+    if (resp === undefined || resp.length === 0) {
+      this.$log('Error while reading snapshot from Framework')
+      return false
+    } else {
+      return true
+    }
+  },
+}
