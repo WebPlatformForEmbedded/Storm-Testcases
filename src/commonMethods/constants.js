d0c4575ff4c7e8e71a2c04200834c631ee2e2c2c
sandeep-vedam <sandeepkumar.vedam@soctronics.com>
2019-11-26 11:32:17 +0530

    Added test cases FrameworkStability 02,03 and commonMethods Folder and resources folder

---
+++ src/commonMethods/constants.js
@@ -0,0 +1,29 @@
+import config from '../../../../config'
+
+export default {
+  //Plugin Names
+  monitorPlugin: 'Monitor',
+  controllerPlugin: 'Controller',
+  webKitBrowserPlugin: 'WebKitBrowser',
+  webKitImplementation: 'WebKitImplementation',
+  deviceInfo: 'DeviceInfo',
+  youTubePlugin: 'YouTube',
+  netFlixPlugin: 'Netflix',
+  ocdmPlugin: 'OCDM',
+  ocdmImplementation: 'OCDMImplementation',
+  snapshotPlugin: 'Snapshot',
+  webServerPlugin: 'WebServer',
+  webServerImplementation: 'WebServerImplementation',
+  provisioningPlugin: 'Provisioning',
+
+  //Plugin states
+  activate: 'activate',
+  deactivate: 'deactivate',
+  resume: 'resumed',
+  suspend: 'suspend',
+
+  WPEFramework: 'WPEFramework',
+
+  blankUrl: 'about:blank',
+  host: config.thunder.host,
+}
