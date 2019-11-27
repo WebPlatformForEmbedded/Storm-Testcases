10ccda83bdc5d3bfd761b25a83838dd9062c3282
sandeep-vedam <sandeepkumar.vedam@soctronics.com>
2019-11-27 18:56:11 +0530

    Added test cases WebKit_Memory_002.test.js

---
+++ src/webkit/WebKit_StartUp_001.test.js
@@ -0,0 +1,52 @@
+import {
+  pluginDeactivate,
+  checkIfProcessIsRunning,
+  getCpuLoad,
+  restartFramework,
+  webKitBrowserOps,
+} from '../commonMethods/commonFunctions'
+import constants from '../commonMethods/constants'
+
+export default {
+  title: 'WPEWebkit startup robustness test',
+  description:
+    'Starts and stops the WPEWebkit plugin repeatedly and checks if everything is started correctly',
+  repeat: 30,
+  teardown: restartFramework,
+  context: {
+    cpuLoad: 90,
+  },
+  steps: [
+    {
+      description: 'Check if WPEWebkit is stopped correctly',
+      test: pluginDeactivate,
+      params: constants.webKitBrowserPlugin,
+      assert: 'deactivated',
+    },
+    {
+      description: 'Check if WebKitImplementation rpcprocess is gone',
+      test: checkIfProcessIsRunning,
+      params: constants.webKitImplementation,
+      assert: false,
+    },
+    {
+      description: 'Activating WebKit Browser Plugin',
+      test: webKitBrowserOps,
+      assert: 'resumed',
+    },
+    {
+      description: 'Get CPU load',
+      test: getCpuLoad,
+      params: constants.deviceInfo,
+    },
+  ],
+  validate() {
+    let cpuload = this.$data.read('cpuload')
+    if (cpuload > 90) {
+      this.$log('CPU load is greater than 90')
+      return false
+    } else {
+      return true
+    }
+  },
+}
