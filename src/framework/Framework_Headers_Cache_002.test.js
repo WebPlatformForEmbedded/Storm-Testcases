540f19f37c2abf67a84b46264a0e87423e1579e2
sandeep-vedam <sandeepkumar.vedam@soctronics.com>
2019-12-02 17:52:01 +0530

    Added test cases related to WPE-464 task in JIRA

---
+++ src/framework/Framework_Headers_Cache_002.test.js
@@ -0,0 +1,79 @@
+import {
+  getPluginInfo,
+  getResponseForURLRequest,
+  restartFramework,
+} from '../commonMethods/commonFunctions'
+import constants from '../commonMethods/constants'
+
+let count = 0
+export default {
+  title: 'Validate cache headers on all plugins',
+  description: 'Iterate over all plugins and verify their cache headers when deactivated',
+  teardown: restartFramework,
+  steps: [
+    {
+      description: 'Check if response is a JSON response',
+      test: getPluginInfo,
+      params: constants.controllerPlugin,
+      validate(result) {
+        this.$data.write('pluginData', result.data)
+        return this.$expect(result).to.be.object() === true
+      },
+    },
+    {
+      description: 'Parse list of modules in the URL',
+      test() {
+        let urls = []
+        let controllerResp = this.$data.read('pluginData')
+        urls.push({ method: 'GET', path: '/Service/Controller' })
+        for (let i = 0; i < controllerResp.plugins.length; i++) {
+          let module = controllerResp.plugins[i].callsign
+          if (module === constants.controllerPlugin) continue
+          if (
+            controllerResp.plugins[i].state.toLowerCase() === 'activated' ||
+            controllerResp.plugins[i].state.toLowerCase() === 'resumed'
+          ) {
+            if (module === 'WebServer' || module === 'WebProxy') {
+              urls.push({ method: 'PUT', path: `/Service/Controller/Deactivate/${module}` })
+              urls.push({ method: 'GET', path: `/Service/Controller/${module}` })
+            } else {
+              urls.push({ method: 'PUT', path: `/Service/Controller/Deactivate/${module}` })
+              urls.push({ method: 'GET', path: `/Service/${module}` })
+            }
+          } else {
+            urls.push({ method: 'GET', path: `/Service/${module}` })
+          }
+        }
+        this.$data.write('urlinfo', urls)
+      },
+    },
+    {
+      description: 'Call all URLs in generated list',
+      repeat() {
+        let urlLength = this.$data.read('urlinfo').length
+        return urlLength - 1
+      },
+      test() {
+        let urlInfo = this.$data.read('urlinfo')
+        let url = `http://${constants.host}:80${urlInfo[count].path}`
+        let methodType = urlInfo[count].method
+        count++
+        return getResponseForURLRequest.call(this, methodType, url)
+      },
+      validate(res) {
+        let headerObj = res.headers
+        let urlInfo = this.$data.read('urlinfo')
+        if (headerObj['cache-control'] === undefined) {
+          this.$log(
+            `Error making request ${urlInfo[count - 1].method} ${urlInfo[count - 1].path}: ${res.error}`
+          )
+          return false
+        } else return true
+      },
+    },
+    {
+      description: 'Restart Framework',
+      test: restartFramework,
+    },
+  ],
+}
