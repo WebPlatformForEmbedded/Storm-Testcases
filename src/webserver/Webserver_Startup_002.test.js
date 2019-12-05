c176e2defd108627f04972a259934bcca899ff0e
sandeep-vedam <sandeepkumar.vedam@soctronics.com>
2019-12-05 19:17:42 +0530

    Added test cases related to WPE-474

---
+++ src/webserver/Webserver_Startup_002.test.js
@@ -0,0 +1,39 @@
+import {
+  checkIfProcessIsRunning,
+  restartFramework,
+  stopWPEFramework,
+  startFramework,
+  pluginActivate,
+} from '../commonMethods/commonFunctions'
+import constants from '../commonMethods/constants'
+
+export default {
+  title: 'WebServer shutdown of Framework robustness test',
+  description: 'Starts WebServer and stops Framework. Checks if everything is shutdown correctly',
+  repeat: 30,
+  teardown: restartFramework,
+  steps: [
+    {
+      description: 'Activating Web Server Plugin',
+      test: pluginActivate,
+      params: constants.webServerPlugin,
+      assert: 'activated',
+    },
+    {
+      description: 'Stop Framework',
+      test: stopWPEFramework,
+      assert: true,
+    },
+    {
+      description: 'Check if WebServerImplementation rpcprocess is gone',
+      test: checkIfProcessIsRunning,
+      params: constants.webServerImplementation,
+      assert: false,
+    },
+    {
+      description: 'Start Framework',
+      test: startFramework,
+      assert: true,
+    },
+  ],
+}
