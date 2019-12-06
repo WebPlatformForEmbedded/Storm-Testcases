3280272898077dbce637666a7fd749eeba802222
sandeep-vedam <sandeepkumar.vedam@soctronics.com>
2019-12-06 20:06:50 +0530

    Added test cases related to the task WPE-476

---
+++ src/framework/Framework_Monitor_001.test.js
@@ -0,0 +1,52 @@
+import {
+  getPluginInfo,
+  pluginDeactivate,
+  pluginActivate,
+  restartFramework,
+} from '../commonMethods/commonFunctions'
+import constants from '../commonMethods/constants'
+
+export default {
+  title: 'Framework Monitor test 001',
+  description: 'Tests if the Framework Monitor module works',
+  teardown: restartFramework,
+  steps: [
+    {
+      description: 'Deactivating Monitor Plugin and checking whether deactivated or not',
+      test: pluginDeactivate,
+      params: constants.monitorPlugin,
+      assert: 'deactivated',
+    },
+    {
+      description: 'Activating Monitor Plugin and checking whether activated or not',
+      test: pluginActivate,
+      params: constants.monitorPlugin,
+      assert: 'activated',
+    },
+    {
+      description: 'Get Monitor Plugin Info',
+      sleep: 5, //This sleep is to make sure that Monitor plugin is activated
+      test: getPluginInfo,
+      params: constants.monitorPlugin,
+      validate(result) {
+        this.$data.write('pluginData', result)
+        return this.$expect(result).to.be.object() === true
+      },
+    },
+  ],
+  validate() {
+    let response = this.$data.read('pluginData')
+    for (let i = 0; i < response.data.length; i++) {
+      let plugin = response.data[i]
+      if (
+        plugin.name === constants.webKitBrowserPlugin ||
+        plugin.name === constants.youTubePlugin
+      ) {
+        return true
+      } else {
+        this.$log('Monitor data is not present for WebKitBrowser and YouTube')
+        return false
+      }
+    }
+  },
+}
