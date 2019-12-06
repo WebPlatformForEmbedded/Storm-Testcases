dd2b6819c61b4895b1876cde6c0a97b6c42f0696
sandeep-vedam <sandeepkumar.vedam@soctronics.com>
2019-12-06 20:09:57 +0530

    Added test cases related to the JIRA task WPE-468

---
+++ src/framework/Framework_Snapshot_001.test.js
@@ -0,0 +1,46 @@
+import {
+  pluginActivate,
+  pluginDeactivate,
+  restartFramework,
+  screenshot,
+  webKitBrowserOps,
+} from '../commonMethods/commonFunctions'
+import constants from '../commonMethods/constants'
+
+export default {
+  title: 'Framework snapshot test',
+  description: 'Tests if the Framework snapshot module works',
+  teardown: restartFramework,
+  steps: [
+    {
+      description: 'Deactivating Snapshot Plugin',
+      test: pluginDeactivate,
+      params: constants.snapshotPlugin,
+      assert: 'deactivated',
+    },
+    {
+      description: 'Activating Snapshot Plugin',
+      test: pluginActivate,
+      params: constants.snapshotPlugin,
+      assert: 'activated',
+    },
+    {
+      description: 'Deactivating and Activating WebKitBrowser plugin',
+      test: webKitBrowserOps,
+      assert: 'resumed',
+    },
+    {
+      description: 'Get a screenshot',
+      test: screenshot,
+    },
+  ],
+  validate() {
+    let resp = this.$data.read('screenshotResult')
+    if (resp === undefined || resp.length === 0) {
+      this.$log('Error while reading snapshot from Framework')
+      return false
+    } else {
+      return true
+    }
+  },
+}
