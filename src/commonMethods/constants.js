4219fd4dfb781b3e0d9fda48903d9d4d673cae08
sandeep-vedam <sandeepkumar.vedam@soctronics.com>
2019-12-26 21:14:51 +0530

    Added test cases as a part of JIRA Task WPE-465

--- src/commonMethods/constants.js
+++ src/commonMethods/constants.js
@@ -1,14 +1,29 @@
+import config from '../../../../config'
+
 export default {
   //Plugin Names
   monitorPlugin: 'Monitor',
   controllerPlugin: 'Controller',
   webKitBrowserPlugin: 'WebKitBrowser',
+  webKitImplementation: 'WebKitImplementation',
+  deviceInfo: 'DeviceInfo',
+  youTubePlugin: 'YouTube',
+  netFlixPlugin: 'Netflix',
+  ocdmPlugin: 'OCDM',
+  ocdmImplementation: 'OCDMImplementation',
+  snapshotPlugin: 'Snapshot',
+  webServerPlugin: 'WebServer',
+  webServerImplementation: 'WebServerImplementation',
+  provisioningPlugin: 'Provisioning',
+  WPEProcess: 'WPEProcess',
+  remoteControlPlugin: 'RemoteControl',
 
   //Plugin states
   activate: 'activate',
   deactivate: 'deactivate',
   resume: 'resumed',
-  suspend: 'suspend',
-
+  suspend: 'suspended',
+  WPEFramework: 'WPEFramework',
   blankUrl: 'about:blank',
+  host: config.thunder.host,
 }
