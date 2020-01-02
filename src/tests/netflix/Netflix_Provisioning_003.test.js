8d1cf624bfe5fbe1637116c363bdd78d41d00b8e
sandeep-vedam <sandeepkumar.vedam@soctronics.com>
2020-01-02 20:33:36 +0530

    Added test cases NetFlixProvisioning003,004 as a part of WPE-502

---
+++ src/tests/netflix/Netflix_Provisioning_003.test.js
@@ -0,0 +1,64 @@
+import {
+  pluginDeactivate,
+  pluginActivate,
+  startProvisioning,
+  getPluginInfo,
+} from '../../commonMethods/commonFunctions'
+import constants from '../../commonMethods/constants'
+
+export default {
+  title: 'Netflix provisioning tests',
+  description:
+    'Validate if Framework does not crash if you start Netflix without the provisioning module being active',
+  steps: [
+    {
+      description: 'Stop Provisioning',
+      test: pluginDeactivate,
+      params: constants.provisioningPlugin,
+      assert: 'deactivated',
+    },
+    {
+      description: 'Stop NetFlix plugin',
+      test: pluginDeactivate,
+      params: constants.netFlixPlugin,
+      assert: 'deactivated',
+    },
+    {
+      description: 'Start Netflix Plugin',
+      test: pluginActivate,
+      params: constants.netFlixPlugin,
+      assert: 'activated',
+    },
+    {
+      description: 'Check if Netflix is started without a valid ESN (screen stays blank)',
+      sleep: 5,
+      test() {
+        return getPluginInfo.call(this, constants.netFlixPlugin)
+      },
+      validate(res) {
+        if (res.status > 400) {
+          this.$log('Netflix plugin is responding with an error')
+        }
+        if (res.data.esn !== undefined && res.data.esn === '' && res.data.esn.length === 0) {
+          return true
+        } else {
+          this.$log('Expected Netflix to not have an ESN, yet it does')
+          return false
+        }
+      },
+    },
+    {
+      description: 'Stop NetFlix plugin',
+      test: pluginDeactivate,
+      params: constants.netFlixPlugin,
+      assert: 'deactivated',
+    },
+    {
+      description: 'Start provisioning',
+      test() {
+        return startProvisioning.call(this)
+      },
+      assert: null,
+    },
+  ],
+}
