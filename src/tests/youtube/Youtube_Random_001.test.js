8fc00db5d83a52b5c3ee5f35f494bcb33cfa66a8
sandeep-vedam <sandeepkumar.vedam@soctronics.com>
2020-01-08 17:32:16 +0530

    Added YoutubeRandom001 and YoutubeRandom002 test cases as a part of WPE-496

---
+++ src/tests/youtube/Youtube_Random_001.test.js
@@ -0,0 +1,94 @@
+import { pluginDeactivate, pluginActivate, screenshot } from '../../commonMethods/commonFunctions'
+import constants from '../../commonMethods/constants'
+
+var keysArray = ['up', 'down', 'left', 'right', 'esc']
+var counter = 0
+let curSameScreenshot = 0
+let maxSameScreenshot = 5
+
+export default {
+  title: 'YouTube Random Key test',
+  description: 'Sends random keys to YouTube and checks if YouTube is still working',
+  steps: [
+    {
+      description: 'Check if WPEWebkit is stopped correctly',
+      test: pluginDeactivate,
+      params: constants.webKitBrowserPlugin,
+      assert: 'deactivated',
+    },
+    {
+      description: 'Check if Youtube is stopped correctly',
+      test: pluginDeactivate,
+      params: constants.youTubePlugin,
+      assert: 'deactivated',
+    },
+    {
+      description: 'Check if Youtube is started correctly',
+      test() {
+        return pluginActivate.call(this, constants.youTubePlugin)
+      },
+      validate(result) {
+        if (result === 'suspended') {
+          return true
+        } else return false
+      },
+    },
+    {
+      description: 'Repeat for 12 hours',
+      repeat: {
+        seconds: 12 * 60 * 60, //Twelve hours
+      },
+      steps: [
+        {
+          description: 'Press keys from Remote Control',
+          repeat: 4,
+          test() {
+            let currCount = counter
+            counter++
+            return this.$thunder.remoteControl.key(keysArray[currCount])
+          },
+          validate(res) {
+            if (res === null) return true
+            else return false
+          },
+        },
+        {
+          description: 'Check if screen still updates',
+          test: screenshot,
+          validate() {
+            let resp = this.$data.read('screenshotResult')
+            if (resp !== undefined && resp.length > 0) {
+              let prevScreenshot = this.$data.read('prevScreenshot')
+              if (
+                prevScreenshot === null ||
+                (prevScreenshot !== null && prevScreenshot.equals(resp) === false)
+              ) {
+                this.$data.write('prevScreenshot', resp)
+                curSameScreenshot = 0
+                return true
+              } else {
+                if (curSameScreenshot >= maxSameScreenshot) {
+                  this.$log(
+                    'Screen is stuck, new screenshot is the same as previous screenshot for ' +
+                      curSameScreenshot +
+                      ' times.'
+                  )
+                  return false
+                }
+                curSameScreenshot++
+                return true
+              }
+            } else {
+              if (curSameScreenshot >= maxSameScreenshot) {
+                this.$log('Error screenshot returned is empty for ' + curSameScreenshot + ' times.')
+                return false
+              }
+              curSameScreenshot++
+              return true
+            }
+          },
+        },
+      ],
+    },
+  ],
+}
