468a81844f98de86bf42b84980307472f67a697f
sandeep-vedam <sandeepkumar.vedam@soctronics.com>
2020-03-03 14:40:35 +0530

    Added test cases related to OCDM plugin

--- src/commonMethods/commonFunctions.js
+++ src/commonMethods/commonFunctions.js
@@ -588,3 +588,24 @@ export const getBluetoothDeviceInfo = function(devicemac) {
     .then(result => result)
     .catch(err => err)
 }
+
+/**
+ * This function is used to get DRMS list
+ * @param devicemac
+ * @returns {Promise<AxiosResponse<any>>}
+ */
+export const getDRMSList = function() {
+  return this.$thunder.api.OCDM.drms()
+    .then(result => result)
+    .catch(err => err)
+}
+/**
+ * This function is used to get DRMS item info
+ * @returns {Promise<AxiosResponse<any>>}
+ * @param keysystem
+ */
+export const getDRMKeySystemInfo = function(keysystem) {
+  return this.$thunder.api.OCDM.keysystems(keysystem)
+    .then(result => result)
+    .catch(err => err)
+}
