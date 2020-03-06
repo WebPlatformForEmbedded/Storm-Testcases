f0c10cddc8c2533210e01ca946271e767ea4b411
sandeep-vedam <sandeepkumar.vedam@soctronics.com>
2020-03-06 20:08:14 +0530

    Added test cases related to Location Sync Module

--- src/commonMethods/commonFunctions.js
+++ src/commonMethods/commonFunctions.js
@@ -495,3 +495,23 @@ export const stopWPEFramework = function() {
   stopProcess('WPEFramework')
   return true
 }
+
+/**
+ * This function is used to sync Location
+ * @returns {Promise<AxiosResponse<any>>}
+ */
+export const syncLocation = function() {
+  return this.$thunder.api.LocationSync.sync()
+    .then(result => result)
+    .catch(err => err)
+}
+
+/**
+ * This function is used to get Location
+ * @returns {Promise<AxiosResponse<any>>}
+ */
+export const getLocation = function() {
+  return this.$thunder.api.LocationSync.location()
+    .then(result => result)
+    .catch(err => err)
+}
