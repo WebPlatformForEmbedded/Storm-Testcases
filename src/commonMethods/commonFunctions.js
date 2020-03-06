5d39ac3f54e389fc5117d722f7ffca3fc853383e
sandeep-vedam <sandeepkumar.vedam@soctronics.com>
2020-03-06 19:01:57 +0530

    Added test cases relate Bluetooth Control Plugin - Devices-Adapters

--- src/commonMethods/commonFunctions.js
+++ src/commonMethods/commonFunctions.js
@@ -495,3 +495,45 @@ export const stopWPEFramework = function() {
   stopProcess('WPEFramework')
   return true
 }
+
+/**
+ * This function is used to get available Bluetooth devices
+ * @returns {Promise<AxiosResponse<any>>}
+ */
+export const getBluetoothDevices = function() {
+  return this.$thunder.api.BluetoothControl.devices()
+    .then(result => result)
+    .catch(err => err)
+}
+
+/**
+ * This function is used to get available Bluetooth adapters
+ * @returns {Promise<AxiosResponse<any>>}
+ */
+export const getBluetoothAdapters = function() {
+  return this.$thunder.api.BluetoothControl.adapters()
+    .then(result => result)
+    .catch(err => err)
+}
+
+/**
+ * This function is used to get info of particular Bluetooth adapters
+ * @param adaptername
+ * @returns {Promise<AxiosResponse<any>>}
+ */
+export const getBluetoothAdapterInfo = function(adaptername) {
+  return this.$thunder.api.BluetoothControl.adapter(adaptername)
+    .then(result => result)
+    .catch(err => err)
+}
+
+/**
+ * This function is used to get info of particular Bluetooth device
+ * @param devicemac
+ * @returns {Promise<AxiosResponse<any>>}
+ */
+export const getBluetoothDeviceInfo = function(devicemac) {
+  return this.$thunder.api.BluetoothControl.adapter(devicemac)
+    .then(result => result)
+    .catch(err => err)
+}
