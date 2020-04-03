a45ba4ab7da7663de802f8e539b5a965f6f17ecd
sandeep-vedam <sandeepkumar.vedam@soctronics.com>
2020-04-03 17:06:07 +0530

    Updated Webkit_Memory_003 test with correct URL

--- src/tests/webkit/WebKit_Stress_003.test.js
+++ src/tests/webkit/WebKit_Stress_003.test.js
@@ -1,67 +1,15 @@
-import baseTest from './WebKit_Stress_001.test'
-import { setWebKitUrl, webKitBrowserStartAndResume } from '../../commonMethods/commonFunctions'
-import fs from 'fs'
-const url = require('url')
+import baseTest from './WebKit_Stress_001.test.js'
 
-let listener
-let data
 export default {
   ...baseTest,
   ...{
+    context: {
+      url: 'http://cdn.metrological.com/static/storm/app_redirect1.html',
+    },
     title: 'WPEWebkit stability redirect test',
     description:
       'Stress loads the system with redirects and see if the WPEWebkit process continues to operate nominally',
-    setup() {
-      return this.$sequence([
-        () => webKitBrowserStartAndResume.call(this),
-        () =>
-          (listener = this.$thunder.api.WebKitBrowser.on('urlchange', data => {
-            this.$data.write('currentUrl', data.url)
-          })),
-        () => (
-          //TODO - Need to update the logic once after redir_app.html is hosted and working
-          (data = fs.readFileSync('./testcases/Storm-Testcases/src/resources/redir_app.html')),
-          this.$data.write('app', data)
-        ),
-      ])
-    },
-    teardown() {
-      listener.dispose()
-    },
     steps: baseTest.steps.map((step, index) => {
-      if (index === 3) {
-        return {
-          description: 'Load the app on WPEWebkit',
-          test() {
-            return setWebKitUrl.call(
-              this,
-              `http://${this.$data.read('server')}:${this.$data.read('port')}`
-            )
-          },
-          validate(res) {
-            return res === `http://${this.$data.read('server')}:${this.$data.read('port')}/`
-          },
-        }
-      }
-      if (index === 4) {
-        return {
-          description: 'Sleep until URL is loaded',
-          sleep() {
-            // Purpose of this sleep is to wait until current step gets 'url change' response from the listener
-            return new Promise((resolve, reject) => {
-              const interval = setInterval(() => {
-                let currentUrl = this.$data.read('currentUrl')
-                var parsedUrl = url.parse(currentUrl)
-                if (parsedUrl.host === `${this.$data.read('server')}:${this.$data.read('port')}`) {
-                  clearInterval(interval)
-                  resolve()
-                }
-                reject('URL not loaded within time limit')
-              }, 1000)
-            })
-          },
-        }
-      }
       return step
     }),
   },
