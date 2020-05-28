2346f1385a07d8aaf10a8a36437b6f5a8a2f94fd
sandeep-vedam <sandeepkumar.vedam@soctronics.com>
2020-05-28 09:54:01 +0530

    Removed suspendtoram, hibernate related tests and renamed few tests

--- src/tests/powerplugin/PowerPlugin_004.test.js
+++ src/tests/powerplugin/PowerPlugin_004.test.js
@@ -1,13 +1,60 @@
-import baseTest from './PowerPlugin_001.test'
+import { setPowerState } from '../../commonMethods/powerPlugin'
+import { pluginActivate, pluginDeactivate } from '../../commonMethods/controller'
+import constants from '../../commonMethods/constants'
 
 export default {
-  ...baseTest,
-  ...{
-    title: 'Power Plugin - 004',
-    description: 'Sets the Power state to suspendtoram and validate the result',
-    context: {
-      powerState: 'suspendtoram',
-      timeOut: 1,
-    },
+  title: 'Power Plugin - 004',
+  description: 'Sets the Power state to already existing power state and validate the result',
+  context: {
+    powerState: 'on',
+    timeOut: 0,
   },
+  steps: [
+    {
+      description: 'Deactivate Power Plugin and check deactivated or not',
+      test: pluginDeactivate,
+      params: constants.powerPlugin,
+      assert: 'deactivated',
+    },
+    {
+      description: 'Activate Power Plugin and check activated or not',
+      test: pluginActivate,
+      params: constants.powerPlugin,
+      assert: 'activated',
+    },
+    {
+      description: 'Set power state and validate the result',
+      test() {
+        return setPowerState.call(
+          this,
+          this.$context.read('powerState'),
+          this.$context.read('timeOut')
+        )
+      },
+      validate(res) {
+        if (res === null) {
+          return true
+        } else {
+          throw new Error('Error while setting the power state')
+        }
+      },
+    },
+    {
+      description: 'Set power state to on and validate the result',
+      test() {
+        return setPowerState.call(
+          this,
+          this.$context.read('powerState'),
+          this.$context.read('timeOut')
+        )
+      },
+      validate(res) {
+        if (res.code === 29 && res.message === 'ERROR_DUPLICATE_KEY') {
+          return true
+        } else {
+          throw new Error('Proper error message is not shown')
+        }
+      },
+    },
+  ],
 }
--- src/tests/powerplugin/PowerPlugin_005.test.js
+++ src/tests/powerplugin/PowerPlugin_005.test.js
@@ -1,13 +1,43 @@
-import baseTest from './PowerPlugin_001.test'
+import { setPowerState } from '../../commonMethods/powerPlugin'
+import { pluginActivate, pluginDeactivate } from '../../commonMethods/controller'
+import constants from '../../commonMethods/constants'
 
 export default {
-  ...baseTest,
-  ...{
-    title: 'Power Plugin - 005',
-    description: 'Sets the Power state to hibernate and validate the result',
-    context: {
-      powerState: 'hibernate',
-      timeOut: 1,
-    },
+  title: 'Power Plugin - 005',
+  description: 'Sets invalid Power state and validate the result',
+  context: {
+    powerState: 'invalid',
+    timeOut: 0,
   },
+  steps: [
+    {
+      description: 'Deactivate Power Plugin and check deactivated or not',
+      test: pluginDeactivate,
+      params: constants.powerPlugin,
+      assert: 'deactivated',
+    },
+    {
+      description: 'Activate Power Plugin and check activated or not',
+      test: pluginActivate,
+      params: constants.powerPlugin,
+      assert: 'activated',
+    },
+    {
+      description: 'Set power state and validate the result',
+      test() {
+        return setPowerState.call(
+          this,
+          this.$context.read('invalid'),
+          this.$context.read('timeOut')
+        )
+      },
+      validate(res) {
+        if (res.code === 2 && res.message === 'ERROR_UNAVAILABLE') {
+          return true
+        } else {
+          throw new Error('Proper error message is not shown')
+        }
+      },
+    },
+  ],
 }
